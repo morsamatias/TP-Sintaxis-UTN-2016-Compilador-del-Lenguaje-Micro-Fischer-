f5590ce4dea65b6495f8931ad3d801c873c58e07
Brian Flores Leibenzon <brian.fl@outlook.com>
2015-09-22 17:05:23 -0300

    Subo todos los cambios

--- src/Funciones.h
+++ src/Funciones.h
@@ -8,6 +8,31 @@
 #include <string.h>
 #include <ctype.h>
 
+
+#include <sys/mman.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+
+#define O_RDONLY         00
+#define O_WRONLY         01
+#define O_RDWR           02
+#define tamanioBloque  20971520
+
+#define NUM_PIPES          2
+
+#define PARENT_WRITE_PIPE  0
+#define PARENT_READ_PIPE   1
+#define READ_FD  0
+#define WRITE_FD 1
+
+#define PARENT_READ_FD  ( pipes[PARENT_READ_PIPE][READ_FD]   )
+#define PARENT_WRITE_FD ( pipes[PARENT_WRITE_PIPE][WRITE_FD] )
+
+#define CHILD_READ_FD   ( pipes[PARENT_WRITE_PIPE][READ_FD]  )
+#define CHILD_WRITE_FD  ( pipes[PARENT_READ_PIPE][WRITE_FD]  )
+
 ///////Defino Variables
 
 typedef enum {
@@ -23,6 +48,18 @@ char buffer[TAMLEX];
 
 TOKEN tokenActual;
 int flagToken;
+
+
+
+
+int	 	  tam;
+char*	  mapeo;
+char* 	  copia_mapeo;
+char* 	  Temp;
+char*     contbloque;
+char*	  hilo_bitmap;
+pthread_t *hilo;
+
 
 //////////////Defino Funciones///////////////////
 
@@ -48,5 +85,10 @@ int columna(int c);
 void AgregarCaracter(int caracter, int posicion);
 TOKEN EsReservada(void);
 void LimpiarBuffer(void);
+
+
+int redireccionar (char* fuente,char* salida,char* script);
+char* copiar_archivo_a_buffer(char* archivo_origen);
+
 
 #endif /* Funciones_H_ */
