de5bdd4af0a8dcd04a5397444318f7a77fa657bf
Brian Flores Leibenzon <brian.fl@outlook.com>
2015-10-18 13:06:04 -0300

    El archivo de salida se pasa opcionalmente

--- archivo.m
+++
@@ -1,7 +0,0 @@
-inicio
-	a:=5;
-	bb:=a+3;
-	leer (a,b);
-	cc := a + b-2;
-	escribir (cc, a+4);
-fin
--- src/Main.c
+++ src/Main.c
@@ -5,33 +5,32 @@
 
 int main(int argc, char* argv[]) {
 
-	if (argc>3){
-
-		fprintf(stderr,"Por favor solo ingrese el archivo fuente y el nombre que desea darle al ejecutable. \n");
-		return (1);
-
-	}else{
-
-		if (argc <3){
-			if (argc==2)
-				fprintf(stderr,"Por favor ingrese el nombre que desea darle al ejecutable.\n");
-			if (argc==1)
-				fprintf(stderr,"Por favor ingrese el nombre del archivo fuente y el nombre que desea darle al ejecutable.\n");
-			return (1);
-		}
+	if (argc!=2 && argc!=3){
+		fprintf(stderr,"Debe ingresar el archivo fuente y opcionalmente el nombre que desea darle al archivo de salida.\n");
+		return -1;
 	}
-	if (validacion(argv[1],'m') && validacion(argv[2],'o')){
-		archivo = fopen(argv[1], "rb");
-		if (archivo == NULL){
-			printf("El archivo no existe\n");
-			return -1;
-		}
+	if (!validacion(argv[1],'m')){
+		printf("La extension del archivo fuente debe ser .m\n");
+		return -2;
+	}
+	archivo = fopen(argv[1], "rb");
+	if (archivo == NULL){
+		printf("El archivo fuente no existe\n");
+		return -3;
+	}
+
+	if (argc==3){
 		archivoSalida = fopen(argv[2], "wb+");
-		Objetivo();
-		fclose(archivo);
-		fclose(archivoSalida);
 	}else{
-		printf("La extension del archivo no es valida\n");
+		archivoSalida = stdout;
 	}
+
+	Objetivo();
+
+	fclose(archivo);
+	if (argc==3){
+		fclose(archivoSalida);
+	}
+
 	return EXIT_SUCCESS;
 }
